aa4cf201404e94bfdf8aef8c301e8d1321a7af41
Weston <weixuhong@gmail.com>
2012-03-25 14:37:10 +0800

    update deploy config
    Signed-off-by: Weston <weixuhong@gmail.com>

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,7 @@
+require "bundler/capistrano"
+
+server "180.168.220.98", :web, :app, :db, primary: true
+
 set :application, "Cooper"
 set :repository,  "git@git.sidways.lab:ruby/outsourcing/cooper"
 set :deploy_to, "/srv/rails/cooper"
@@ -5,9 +9,9 @@ set :deploy_to, "/srv/rails/cooper"
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "aries.sidways.lab"                          # Your HTTP server, Apache/etc
-role :app, "aries.sidways.lab"                          # This may be the same as your `Web` server
-role :db,  "aries.sidways.lab", :primary => true # This is where Rails migrations will run
+# role :web, "aries.sidways.lab"                          # Your HTTP server, Apache/etc
+# role :app, "aries.sidways.lab"                          # This may be the same as your `Web` server
+# role :db,  "aries.sidways.lab", :primary => true # This is where Rails migrations will run
 # role :db,  "your slave db-server here"
 
 set :user, "rails"
@@ -15,6 +19,12 @@ set :user, "rails"
 set :use_sudo, false
 
 set :branch, "master"
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
+
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
@@ -28,13 +38,26 @@ set :branch, "master"
    end
  end
  
+ task :setup_config, roles: :app do
+   # sudo "ln -nfs #{current_path}/config/apache.conf /etc/apache2/sites-available/#{application}"
+   run "mkdir -p #{shared_path}/config"
+   put File.read("config/database.yml.mysql"), "#{shared_path}/config/database.yml"
+   puts "Now edit the config files in #{shared_path}."
+ end
+ 
+ after "deploy:setup", "deploy:setup_config"
+ 
+ task :symlink_config, roles: :app do
+   run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+ end
+ after "deploy:finalize_update", "deploy:symlink_config"
  
  # 设置数据库
- namespace :deploy do
-   task :config_database do 
-     run "#{try_sudo} cp #{File.join(current_path,'config','database.yml.mysql')}  #{File.join(current_path,'config','database.yml')}"
-   end
- end
+ # namespace :deploy do
+ #   task :config_database do 
+ #     run "#{try_sudo} cp #{File.join(current_path,'config','database.yml.mysql')}  #{File.join(current_path,'config','database.yml')}"
+ #   end
+ # end
  
  namespace :deploy do
    task :bundle_install do 
