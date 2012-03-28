47467ab7973a065c1e5126e5b09a74c51b532042
Weston <weixuhong@gmail.com>
2012-03-28 14:04:59 +0800

    update downloads
    Signed-off-by: Weston <weixuhong@gmail.com>

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 class DownloadsController < ApplicationController
   # before_filter :authenticate
-  http_basic_authenticate_with :name => "media.cooper", :password => "cooperchina"
+  http_basic_authenticate_with :name => "media.cooper", :password => "cooperchina", :realm => "Cooper"
 
   def index
     @files = Refinery::Resource.where("media_type = '新闻通稿'")
