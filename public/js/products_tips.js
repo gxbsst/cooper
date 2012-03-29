0ee33412e925ab6f2e94c00d813b3cea356ccc15
ying.xiao <ying.xiao@sidways.com>
2012-03-29 09:36:28 +0800

    template modify

--- public/js/products_tips.js
+++ public/js/products_tips.js
@@ -4,15 +4,29 @@ function()
   $("#products_tips dt").click(
   function()
   {
-    $("#products_tips dl").removeClass("current");
-    $(this).parent().addClass("current");
+    if($(this).parent().html() == $("#products_tips .current").html())
+    {
+      $(this).parent().removeClass("current");
+    }
+    else
+    {
+     $("#products_tips dl").removeClass("current");
+     $(this).parent().addClass("current");
+    }
   }
   );
   $("#recruitcontent dt").click(
   function()
   {
-    $("#recruitcontent dl").removeClass("current");
-    $(this).parent().addClass("current");
+   if($(this).parent().html() == $("#recruitcontent .current").html())
+   {
+      $(this).parent().removeClass("current");
+   }
+   else
+   {
+     $("#recruitcontent dl").removeClass("current");
+     $(this).parent().addClass("current");
+   }
   }
   );
 }
